3e29881d9f6b138a4c77cb81783c8881d2d8b2f6
Md Mahedi Hasan <mdmahedi6427@gmail.com>
2026-04-01 23:12:01 +0600

    added the modal and functionality

--- script/index.js
+++ script/index.js
@@ -3,6 +3,12 @@ const loadLessons = () =>{
     .then(res=> res.json()) //promise of json data
     .then((json) => displayLesson(json.data))
 }
+const removeActive =()=> {
+    const lessonButtons = document.querySelectorAll(".lesson-btn")
+    // console.log(lessonButtons)
+    lessonButtons.forEach((btn) => btn.classList.remove("active"))
+
+}
 
 const loadLevelWord = (id) => {
 
@@ -10,13 +16,30 @@ const loadLevelWord = (id) => {
     // console.log(url);
     fetch(url)
     .then((res) => res.json())
-    .then((data) =>displayLevelWord(data.data))
+    .then((data) =>{
+        removeActive() // remove all active class
+        const clickBtn = document.getElementById(`lesson-btn-${id}`)
+        // console.log(clickBtn)
+        clickBtn.classList.add('active') // add active class
+
+        displayLevelWord(data.data)
+
+    })
 }
 
 const displayLevelWord = (words)=>{
     // console.log(words)
     const wordContainer = document.getElementById('word-container');
-    wordContainer.innerHTML = ''
+    if (words.length == 0){
+        wordContainer.innerHTML = 
+        `
+        <div class="text-center col-span-full space-y-6 rounded-xl py-10 font-bangla">
+        <img class=mx-auto src="./assets/alert-error.png" alt="">
+        <p class="text-xl text-gray-400 font-medium ">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+      </div>
+        `
+    }
 
 //     {
 //     "id": 82,
@@ -26,18 +49,19 @@ const displayLevelWord = (words)=>{
 //     "pronunciation": "কার"
 // }
 
+
     words.forEach((word)=>{
         console.log(word)
         const card = document.createElement('div')
         card.innerHTML = `
         
       <div class="bg-white rounded-xl shadow-sm text-center px-5 py-10 space-y-4">
-        <h2 class="font-bold text-2xl">${word.word}</h2>
+        <h2 class="font-bold text-2xl">${word.word?word.word : 'no word found'}</h2>
         <p class="font-semibold">Meaning /Pronounciation</p>
-        <div class="text-2xl font-bangla font-medium ">"${word.meaning} / ${word.pronunciation}"</div>
+        <div class="text-2xl font-bangla font-medium ">"${word.meaning ?word.meaning : "no found word meaning"} / ${word.pronunciation?word.pronunciation: "not found word pronunciation"}"</div>
         <div class="flex justify-between items-end">
-          <button class="btn  bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
-          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+          <button  class="btn  bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+          <button onclick="my_modal_5.showModal()" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
         </div>
       </div>`
         wordContainer.append(card);
@@ -61,10 +85,9 @@ const displayLesson = (lessons) => {
     //     3.create Element
          const btnDiv = document.createElement("div")
     btnDiv.innerHTML = `
-        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary " ><i class="fa-solid fa-book-open"></i>lesson ${lesson.level_no}</button>
-    
+        <button id='lesson-btn-${lesson.level_no}' onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn" ><i class="fa-solid fa-book-open"></i>lesson ${lesson.level_no}</button>
     `
-    //     4.append into container
+    // 4.append into container
     levelContainer.append(btnDiv)
     } 
 }
